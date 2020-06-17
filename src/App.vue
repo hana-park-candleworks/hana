080c5a248e65c1898bea0ff8e9cd0b0f1b61be14
Hana Park <hana_park@candleworks.co.kr>
2020-06-17 11:03:19 +0900

    router link design 수정

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="text-center bg-green-300">
-        <ul>
-            <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="/about">About</router-link></li>
-            <li><router-link to="/to-do-list">To Do List</router-link></li>
+    <div class="bg-black text-white">
+        <ul class="flex text-center">
+            <li class="flex-1 px-4 py-2 m-2"><router-link to="/">Home</router-link></li>
+            <li class="flex-1 px-4 py-2 m-2"><router-link to="/about">About</router-link></li>
+            <li class="flex-1 px-4 py-2 m-2"><router-link to="/to-do-list">To Do List</router-link></li>
         </ul>
         <router-view></router-view>
     </div>
