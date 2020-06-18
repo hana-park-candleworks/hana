a3fd21fc244c1f75e9588074081f15fb82ca7f50
Hana Park <hana_park@candleworks.co.kr>
2020-06-18 10:25:03 +0900

    Vue Mastery Practice - The Vue Instance - 바인딩 속성 사용하기 - a tag link 이동하기 - 조건문 넣어서 조건 대로 문구 나오게 하기 - 클릭으로 문구 block/none 왔다갔다 하기

--- src/components/login/LoginHeader.vue
+++ src/components/login/LoginHeader.vue
@@ -1,12 +1,30 @@
 <template>
     <div class="text-black border-b-2 border-black">
         <h1>로그인</h1>
-        <span>어서오세요. DIY 홈 프레그런스의 가장 깊이 있는 경험이 있는 곳, 캔들웍스 입니다.</span>
+        <span>
+            {{ message }}
+            <i class="fa fa-arrow-right" @click="addCount"/>
+        </span>
+        <a v-if="countRemain === 0" :href="candleUrl">캔들웍스 이동하기</a>
     </div>
 </template>
 
 <script>
     export default {
-        name: "LoginHeader.vue"
+        name: "LoginHeader.vue",
+        data() {
+            return {
+                message: '어서오세요. DIY 홈 프레그런스의 가장 깊이 있는 경험이 있는 곳, 캔들웍스입니다.',
+                candleUrl: "https://candleworks.co.kr/shop/login.php",
+                count: 0,
+                countRemain: 1
+            }
+        },
+        methods:{
+            addCount() {
+                this.count += 1
+                this.countRemain = this.count % 2
+            }
+        }
     }
 </script>
