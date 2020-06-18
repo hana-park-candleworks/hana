8b97b25c1edf17d22ffc9f4779a952da2fcf3750
Hana Park <hana_park@candleworks.co.kr>
2020-06-18 13:46:09 +0900

    LoginMember.vue style 적용

--- src/components/login/LoginMember.vue
+++ src/components/login/LoginMember.vue
@@ -2,35 +2,40 @@
     <section class="width-lg" style="margin-bottom: 40px">
         <div class="text-black">
             <div class="flex space-between">
-                <div class="flex flex-col" :xs="24">
-                    <h4>회원</h4>
-                    <form style="margin-bottom: 20px">
-                       <input placeholder="아이디">
-                        <input placeholder="패스워드" type="password">
-                    </form>
-                    <div>
-                        <button style="width: 100%">로그인</button>
-                    </div>
-                    <div>
-                        <input type="checkbox">자동 로그인
-                        <span>아이디/비밀번호 찾기</span>
-                    </div>
-                </div>
-                <div class="flex flex-col">
-                    <h4>비회원</h4>
+                <div class="flex-col w-1/2 p-3 pl-0 " :xs="24">
+                    <h4 class="subtitle">회원</h4>
                     <form style="margin-bottom: 20px">
                         <div>
-                            <input placeholder="주문번호">
+                            <input class="input-style" placeholder="아이디">
                         </div>
                         <div style="margin-bottom: 10px">
-                            <input placeholder="비밀번호" type="password">
+                            <input class="input-style" placeholder="패스워드" type="password">
                         </div>
                     </form>
                     <div>
-                        <button style="width: 100%">주문조회</button>
+                        <button class="button-style">로그인</button>
                     </div>
                     <div>
-                        <h6 style="margin-top: 20px; margin-bottom: 20px; height: 30px;">아직 캔들웍스의 회원이 아니세요?</h6>
+                        <input type="checkbox">
+                        <span class="checkBox-style">자동 로그인</span>
+                        <span class="checkBox-style">아이디/비밀번호 찾기</span>
+                    </div>
+                </div>
+                <div class="flex-col w-1/2 p-3 pl-0">
+                    <h4 class="subtitle">비회원</h4>
+                    <form style="margin-bottom: 20px">
+                        <div>
+                            <input class="input-style" placeholder="주문번호">
+                        </div>
+                        <div style="margin-bottom: 10px">
+                            <input class="input-style" placeholder="비밀번호" type="password">
+                        </div>
+                    </form>
+                    <div>
+                        <button class="button-style">주문조회</button>
+                    </div>
+                    <div>
+                        <h6 style="margin-top: 20px; margin-bottom: 20px; height: 30px; font-size: 13px;">아직 캔들웍스의 회원이 아니세요?</h6>
                     </div>
                 </div>
             </div>
@@ -45,7 +50,30 @@
 </script>
 
 <style scoped>
-
+    .subtitle {
+        font-size: 20px;
+        font-weight: bold;
+        margin-bottom: 20px;
+    }
+    .input-style {
+        border: 1px solid #ddd;
+        font-size: 11px;
+        padding: 0.3rem 0.5rem;
+        width: 100%;
+        margin-bottom: 20px;
+    }
+    .button-style {
+        width: 100%;
+        color: white;
+        font-size: 11px;
+        padding: 0.3rem 0.5rem;
+        border: none;
+        background: dimgrey;
+    }
+    .checkBox-style{
+        font-size: 11px;
+        margin: 0.3rem;
+    }
 
 
 </style>
